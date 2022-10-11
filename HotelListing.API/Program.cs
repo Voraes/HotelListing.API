0417e33e6d9621ea241fa683216c1047468c0176
Victor Moraes Mendes <victor.inscriptio@gmail.com>
2022-10-10 17:00:09 -0700

    Add Entity Framework and create DataBase

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -7,11 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //Adding DbContext with EntityFramework
-builder.Services.AddDbContext<HotelListingDbContext>(options =>
-    options.UseSqlServer("HotelListingDbConnectionString"));
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options => {
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
